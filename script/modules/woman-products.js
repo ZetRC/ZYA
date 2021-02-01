15372d2199d49d647b16a8ee2ecce4bfff811ac7
zetRC <zetrosas555@gmail.com>
2021-02-01 10:32:59 -0600

    added bg data toggler in products cards

--- script/modules/woman-products.js
+++ script/modules/woman-products.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
     var loadProductsGrid=(()=>{
         fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
+            //builds products grid
             .then(data=>{
                 if(data.length > 0){
                   var cont="";
-
+                  console.log(data)
+                  console.log(data[0].id)
                   data.forEach((u)=>{
-                    cont +=`<div id='product-element ' class='product-element col-xl-3 col-lg-4 col-6 '>`
+                    cont +=`<div id='product-element ' class='product-element col-xl-3 col-lg-4 col-6 ' data-id="${u.id}">`
                     cont +=`<img src='${u.image}'>`
                     cont +=`<div class='productName'>${u.title}</div>`
                     cont +=`<div class='productPrice'>$${u.price}</div>`
@@ -16,9 +18,25 @@ $(document).ready(function(){
                 }else{
                   console.log("something went wrong")
                 }
-
                 document.getElementById("prod-cont-woman").innerHTML=cont
+                //builds product cards        
+                $(".product-element , #close-card-btn , #cards-dark-bg").click(function(){
+                  $("#productCard").fadeToggle()
+                  $("body").toggleClass('modal-open');
+                  
+                })          
+
+                $(".product-element").click(function(){
+                  var dataId = $(this).attr("data-id");
+
+                  $(".product-image").html(`<img src="${data[dataId-1].image}">`)
+                  $(".product-category").html(data[dataId-1].category)
+                  $(".product-name").html(data[dataId-1].title)
+                  $(".product-price").html("$"+data[dataId-1].price)
+                  $(".product-description").html(data[dataId-1].description)
+                })                  
               })
+              //turns off loader after request is done 
               .then(()=>{
                 $("#product-grid").css("display","block")
                 $("#productsLoader").css("display","none")
@@ -37,18 +55,6 @@ $(document).ready(function(){
       })
     })
 
-    var openProductCar=(()=>{
-      setTimeout(()=>{
-        $(".product-element , #close-card-btn , #productCard").click(function(){
-          $("#productCard").fadeToggle()
-          $("body").toggleClass('modal-open');
-        })
-      },3000)
-      
-      
-    })
-
-    openProductCar()
     initFiltersToggler()
     initNavbarToggler()
     loadProductsGrid()
