eba6a46287242a431b3e8367b20be60013b15074
zetRC <zetrosas555@gmail.com>
2021-02-01 11:22:02 -0600

    added product cards in home

--- script/modules/script.js
+++ script/modules/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
                   var cont="";
 
                   data.forEach((u)=>{
-                    cont +=`<div id='product-element ' class='product-element '>`
+                    cont +=`<div id='product-element ' class='product-element ' data-id="${u.id}">`
                     cont +=`<img src='${u.image}'>`
                     cont +=`<div class='productName'>${u.title}</div>`
                     cont +=`<div class='productPrice'>$${u.price}</div>`
@@ -23,9 +23,26 @@ $(document).ready(function(){
                 
                 document.getElementById("prod-cont").innerHTML=cont
                 document.getElementById("prod-cont-2").innerHTML=cont
+              //builds products cards
+
+                $(".product-element , #close-card-btn , #cards-dark-bg").click(function(){
+                  $("#productCard").fadeToggle()
+                  $("body").toggleClass('modal-open');
+                  
+                })          
+                
+                $(".product-element").click(function(){
+                  var dataId = $(this).attr("data-id");
+  
+                  $(".product-image").html(`<img src="${data[dataId-1].image}">`)
+                  $(".product-category").html(data[dataId-1].category)
+                  $(".product-name").html(data[dataId-1].title)
+                  $(".product-price").html("$"+data[dataId-1].price)
+                  $(".product-description").html(data[dataId-1].description)
+                })                  
               })
               .then(()=>{
-                
+                //init slick stripes
                 $('#prod-cont , #prod-cont-2').slick({
                     infinite: true,
                     speed: 300,
