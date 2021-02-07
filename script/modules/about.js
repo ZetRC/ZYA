01ccdc4c60659dc1767dcad4f4033b4aae552dbc
zetRC <zetrosas555@gmail.com>
2021-02-07 17:41:01 -0600

    added navbar toggler and fixed small layout issues in about page

---
+++ script/modules/about.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var initNavbarToggler=(()=>{
+        $("#menu-toggler").click(function(){
+          $("#menu-cont").slideToggle()
+        })
+      })
+
+      initNavbarToggler()
+})
